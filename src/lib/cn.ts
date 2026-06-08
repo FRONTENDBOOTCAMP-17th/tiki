7bbaf88e942b3cf2eed178bd25aec900b010527c
zzz664 <temperis0512@gmail.com>
2026-06-08 14:30:15 +0900

    feat(Button): 버튼 구현을 위한 의존성 설치  및 유틸함수 작성
    clsx, tailwind-merge, class-variance-authority 의존성을 설치했음
    clsx -> 조건부로 클래스 이름을 결합하기 위함
    tailwind-merge -> 중복된 tailwind class를 제거, 클래스 충돌 문제를 해결하기 위함
    class-variance-authority -> variant, size 등의 props로 다양한 기본 스타일을 제공하기 위함
    
    cn.ts -> clsx, tailwind-merge를 조합하여 두 기능을 한번에 수행하도록 하는 유틸리티함수 cn을 작성

---
+++ src/lib/cn.ts
@@ -0,0 +1,7 @@
+//className 병합 유틸 함수
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
