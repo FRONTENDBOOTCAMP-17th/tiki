b0ab9d9cfc30062193463e9f15e97fb003c15c60
zzz664 <temperis0512@gmail.com>
2026-06-09 00:19:52 +0900

    chore(lucide-react): 아이콘 사용을 위한 lucide-react 의존성 설치

---
+++ src/app/example/toast/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Button from '@/components/Button';
+import useToast from '@/hooks/useToast';
+
+export default function ButtonExamplePage() {
+  const toast = useToast();
+  return (
+    <main className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+      <p className='mt-2 text-muted-foreground'>Toast 컴포넌트</p>
+
+      <section className='space-y-4'>
+        <Button onClick={() => toast.success('성공 메시지!')}>성공</Button>
+        <Button onClick={() => toast.error('에러 메시지!', 1000)}>에러</Button>
+        <Button onClick={() => toast.warning('경고 메시지!')}>경고</Button>
+        <Button onClick={() => toast.info('정보 메시지!')}>정보</Button>
+      </section>
+    </main>
+  );
+}
