553a9e10f56bc41e0a40251f0f1c1ecc0697de50
zzz664 <temperis0512@gmail.com>
2026-06-09 16:38:10 +0900

    feat(Toast): 토스트 컨테이너 추가 수정
    컨테이너의 position에 따라 적용되는 애니메이션이나 스택 레이아웃을 다르게 적용함
    toasts 배열에는 최신 토스트가 맨 뒤에 위치하므로 배열을 뒤집어서 토스트를 렌더링 할 수 있도록 함
    순서에 따라 최신 토스트가 맨 위에 보일 수 있도록 z-index를 설정함

--- src/components/ToastContainer.tsx
+++ src/components/ToastContainer.tsx
@@ -1,20 +1,43 @@
+'use client';
+
+import { cn } from '@/lib/cn';
 import Toast from './Toast';
-import { ToastData } from './Toast.types';
+import { ToastData, ToastPosition } from './Toast.types';
+import { toastPositionMap } from './Toast.position';
+import useToast from '@/hooks/useToast';
 
 export default function ToastContainer({ toasts }: { toasts: ToastData[] }) {
+  const { position } = useToast();
+  const displayToasts = [...toasts].reverse();
+
+  const getContainerLayoutStyle = (position: ToastPosition, index: number) => {
+    const style: React.CSSProperties = {
+      zIndex: displayToasts.length * 10 - index,
+    };
+
+    if (position.split('-')[0] === 'top')
+      style.transform = `translateY(${index * -10}px) scale(${1 - index * 0.1})`;
+    else
+      style.transform = `translateY(${index * 10}px) scale(${1 - index * 0.1})`;
+    return style;
+  };
   return (
-    <div className='fixed top-4 right-4 z-50 flex flex-col gap-2'>
-      {toasts.map((toast) => {
-        return (
-          <Toast
-            key={toast.id}
-            id={toast.id}
-            title={toast.title}
-            description={toast.description}
-            variant={toast.variant}
-          />
-        );
-      })}
+    <div className={cn('fixed z-10', toastPositionMap[position])}>
+      <div className='relative min-w-100'>
+        {displayToasts.map((toast, index) => {
+          return (
+            <Toast
+              style={getContainerLayoutStyle(position, index)}
+              key={toast.id}
+              id={toast.id}
+              title={toast.title}
+              description={toast.description}
+              variant={toast.variant}
+              isClosing={toast.isClosing}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
