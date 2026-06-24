7848013dcb97a4c106eb7d5c5c96259ac1ed948b
zzz664 <temperis0512@gmail.com>
2026-06-24 14:19:42 +0900

    chore(toast): 토스트 출력위치를 하단중앙배치로 변경, 애니메이션을 슬라이드로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className='min-h-full flex flex-col'>
-        <ToastProvider>{children}</ToastProvider>
+        <ToastProvider animation='slide' position='bottom-center'>
+          {children}
+        </ToastProvider>
       </body>
     </html>
   );
